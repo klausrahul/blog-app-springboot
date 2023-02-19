8a16f3672a2c05113ff5f8e04b98a819c50287a7
klausrahul <rahulmittal2805@gmail.com>
2023-02-20 02:03:17 +0530

    Second Form eclipe

--- src/main/java/com/rahul/blog/app/config/ContentConfig.java
+++ src/main/java/com/rahul/blog/app/config/ContentConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ContentConfig implements WebMvcConfigurer {
-
+//hi
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		configurer.favorParameter(true)
